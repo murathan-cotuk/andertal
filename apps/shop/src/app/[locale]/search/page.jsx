e8d1c9ac78ef784aaa7fd25db1d33caeb774965b
Murathan Cotuk <murathan.cotuk@gmail.com>
2026-03-20 09:21:35 +0100

    UI

--- apps/shop/src/app/[locale]/search/page.jsx
+++ apps/shop/src/app/[locale]/search/page.jsx
@@ -1,7 +1,12 @@
 "use client";
 
+import React, { Suspense } from "react";
 import SearchTemplate from "@/components/templates/SearchTemplate";
 
 export default function SearchPage() {
-  return <SearchTemplate />;
+  return (
+    <Suspense fallback={<div style={{ padding: 48, color: "#6b7280", textAlign: "center" }}>Loading...</div>}>
+      <SearchTemplate />
+    </Suspense>
+  );
 }
